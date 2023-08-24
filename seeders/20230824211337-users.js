0f697e22fbd717c49ac5dda0eb82f3be848bfb67
Erick Amorim <101286204+Amorim-EA@users.noreply.github.com>
2023-08-24 17:41:00 -0400

    atualizei os arquivos

--- seeders/20230824211337-users.js
+++ seeders/20230824211337-users.js
@@ -3,18 +3,29 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('pessoas', [
-     
+    await queryInterface.bulkInsert('users', [
       {
         name: 'John Doe',
-        password: 'senha123',
+        password: 'senha111',
         email: 'jonh@gmail.com',
-        telefone: '6799504-2434'
+        telefone: '6799785-2434'
+      },
+      {
+        name: 'John Amorim',
+        password: 'senha222',
+        email: 'amorim@gmail.com',
+        telefone: '6799844-2434'
+      },
+      {
+        name: 'John da Silva',
+        password: 'senha333',
+        email: 'silva@gmail.com',
+        telefone: '6799554-2434'
       },
     ], {});
   },
 
   async down (queryInterface, Sequelize) {
-   await queryInterface.bulkDelete('pessoas', null, {});
+   await queryInterface.bulkDelete('users', null, {});
   }
 };
